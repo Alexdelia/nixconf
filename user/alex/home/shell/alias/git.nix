8f08c50025aa469643e4349f6a06a6476ff74d5f
adelille <adelille@student.42.fr>
2024-06-23 17:09:00 +0200

    some `git` `alias`

--- user/alex/home/shell/alias/git.nix
+++ user/alex/home/shell/alias/git.nix
@@ -1,3 +1,26 @@
 {
+  gg = "git clone --recurse-submodules";
+
   gst = "git status --short";
+  gs = "git status --short";
+  gd = "git diff";
+
+  gi = "git log --graph --stat --all \
+  --pretty='%n%C(magenta)╭╴%C(auto)%h%C(magenta)╶╴%C(bold green)%an%Creset%C(magenta)╶╴%C(green)%ae%C(magenta)╶╴%C(bold blue)%ch%Creset%n%C(magenta)╰╴%C(auto)%s%Creset'";
+  glast = "git --no-pager log -n1 \
+  --pretty='%C(magenta)╭╴%C(auto)%h%C(magenta)╶╴%C(bold green)%an%Creset%C(magenta)╶╴%C(green)%ae%C(magenta)╶╴%C(bold blue)%ch%Creset%n%C(magenta)╰╴%C(auto)%s%Creset'";
+  gtree = "git log --graph --oneline --decorate --all";
+
+  gc = "git commit --message";
+
+  gl = "git pull";
+  gp = "git push";
+  gf = "git fetch";
+
+  go = "git checkout";
+  gb = "git branch";
+
+  gm = "git merge";
+
+  gk = "git cherry-pick";
 }
