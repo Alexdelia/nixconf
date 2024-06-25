8bdcec0c7b7dd2701cf344fa5040162042d47756
Alexdelia <alexandre.delille.57@gmail.com>
2024-06-25 22:59:37 +0200

    locale

---
+++ system/locale.nix
@@ -0,0 +1,18 @@
+{
+  i18n.defaultLocale = "en_UK.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_TIME = "fr_FR.UTF-8"; # use an english + dd/mm/yyyy format
+    LC_NUMERIC = "mfe.UTF-8";
+    LC_MONETARY = "mfe.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+
+    LC_NAME = "en_US.UTF-8";
+
+    LC_PAPER = "nl_NL.UTF-8";
+  };
+}
