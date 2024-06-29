cb974b18a7a34177d4088be70523707176dfac38
Alexdelia <alexandre.delille.57@gmail.com>
2024-06-29 17:01:43 +0200

    update locales

--- system/locale.nix
+++ system/locale.nix
@@ -1,23 +1,23 @@
 {
-  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8/UTF-8";
 
   i18n.extraLocaleSettings = {
     # 31/01/1970 24:59:59
     LC_TIME = "en_SG.UTF-8";
     # 1 234.56
-    LC_NUMERIC = "mfe_MU.UTF-8";
-    # ₹1234.56
-    LC_MONETARY = "en_IN.UTF-8";
+    LC_NUMERIC = "mfe_MU";
+    # € 1234,56
+    LC_MONETARY = "nl_NL.UTF-8/UTF-8";
     # meter system
-    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8/UTF-8";
 
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_ADDRESS = "fr_FR.UTF-8/UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8/UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8/UTF-8";
 
-    LC_NAME = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8/UTF-8";
 
-    LC_PAPER = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8/UTF-8";
   };
 
   i18n.supportedLocales = [
