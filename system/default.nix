6ce4e904e0746b782d4aef8c498dfba88936622e
Alexdelia <alexandre.delille.57@gmail.com>
2024-07-06 23:01:03 +0200

    `nix gc`

--- system/default.nix
+++ system/default.nix
@@ -4,7 +4,15 @@
     ./de
   ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings.experimental-features = ["nix-command" "flakes"];
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
 
   nixpkgs.config.allowUnfree = true;
 }
