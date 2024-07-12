ff67a9eec5f8269fff5a693d54ac1bc3246e8082
Alexdelia <alexandre.delille.57@gmail.com>
2024-07-12 22:34:56 +0200

    memory usage

--- user/alex/home/shell/starship/memory_usage.nix
+++ user/alex/home/shell/starship/memory_usage.nix
@@ -1,5 +1,10 @@
 {
   disabled = false;
 
-  format = "$symbol [\${ram}( | \${swap})]($style)\n";
+  format = "  [[$symbol](bold green)  $ram_pct $ram( | $swap $swap_pct)]($style)\n";
+  style = "bold red";
+
+  symbol = "";
+
+  threshold = 50;
 }
