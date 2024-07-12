8e645573eeebdb2ae26a1a03b0cca903df55950d
Alexdelia <alexandre.delille.57@gmail.com>
2024-07-12 21:50:52 +0200

    `git commit`

--- user/alex/home/shell/starship/default.nix
+++ user/alex/home/shell/starship/default.nix
@@ -10,12 +10,14 @@
           "$cmd_duration"
           "$directory"
           "$git_branch"
+          "$git_commit"
           " "
         ];
 
         cmd_duration = import ./cmd_duration.nix;
         directory = import ./directory.nix;
         git_branch = import ./git_branch.nix;
+        git_commit = import ./git_commit.nix;
       };
     };
   };
