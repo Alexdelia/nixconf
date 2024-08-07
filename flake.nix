f956c11b5650e98590acf4a1b71ada499972bf34
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-07 21:34:44 +0200

    working filter

--- flake.nix
+++ flake.nix
@@ -12,77 +12,37 @@
 
   outputs = inputs: let
     hosts = {
-      # decim = "x86_64-linux";
-      work = "aarch64-linux";
-      melons = "x86_64-linux";
-      qemu = "x86_64-linux";
-    };
-  in (import ./host {
-    inherit inputs;
-    hosts = {
+      /*
+      decim = {
+        system = "x86_64-linux";
+        stateVersion = "24.05";
+        isNixos = true;
+        users = ["alex"];
+      };
+      */
       melons = {
         system = "x86_64-linux";
         stateVersion = "24.05";
         isNixos = true;
         users = ["alex"];
       };
+      work = {
+        system = "aarch64-linux";
+        stateVersion = "24.05";
+        isNixos = false;
+        users = ["alex"];
+      };
+      /*
+      qemu = {
+        system = "x86_64-linux";
+        stateVersion = "23.11";
+        isNixos = true;
+        users = ["alex"];
+      };
+      */
     };
+  in (import ./host {
+    inherit inputs;
+    inherit hosts;
   });
-  /*
-  nixosConfigurations = (
-    builtins.mapAttrs (
-      host: system:
-        inputs.nixpkgs.lib.nixosSystem {
-          inherit system;
-
-          modules = [
-            inputs.nixosModules.home-manager
-
-            ./host/${host}
-            {
-              _module.args = {
-                hostname = host;
-                inherit inputs;
-              };
-            }
-          ];
-        }
-    )
-    hosts
-  );
-
-  homeConfigurations = (
-    builtins.map (
-      username: ilib.mkStandaloneHome {inherit username system stateVersion;}
-    )
-    users
-  );
-  */
-
-  /*
-  nixosConfigurations =
-    builtins.mapAttrs (
-      name: system:
-        nixpkgs-stable.lib.nixosSystem {
-          inherit system;
-
-          modules = [
-            home-manager.nixosModules.home-manager
-
-            ./host/${name}
-            {
-              _module.args = {
-                hostname = name;
-                inherit nixpkgs-unstable;
-                inherit home-manager;
-              };
-            }
-          ];
-        }
-    ) {
-      # decim = "x86_64-linux";
-      qemu = "x86_64-linux";
-      work = "aarch64-linux";
-    };
-  */
 }
--- host/default.nix
+++ host/default.nix
@@ -5,52 +5,46 @@
   nixosConfigurations =
     builtins.mapAttrs (
       hostname: hostAttrs: (
-        if !hostAttrs.isNixos
-        then null
-        else
-          inputs.nixpkgs.lib.nixosSystem {
-            system = hostAttrs.system;
+        inputs.nixpkgs.lib.nixosSystem {
+          system = hostAttrs.system;
 
-            modules = [
-              inputs.home-manager.nixosModules.home-manager
+          modules = [
+            inputs.home-manager.nixosModules.home-manager
 
-              ./${hostname}
-              {
-                _module.args = {
-                  inherit inputs;
-                  inherit hostname;
-                  stateVersion = hostAttrs.stateVersion;
-                };
-              }
-            ];
-          }
+            ./${hostname}
+            {
+              _module.args = {
+                inherit inputs;
+                inherit hostname;
+                stateVersion = hostAttrs.stateVersion;
+              };
+            }
+          ];
+        }
       )
     )
-    hosts;
+    (inputs.nixpkgs.lib.filterAttrs (hostname: hostAttrs: hostAttrs.isNixos) hosts);
 
   homeConfigurations = inputs.nixpkgs.lib.flatten (
     inputs.nixpkgs.lib.mapAttrsToList (
       hostname: hostAttrs:
-        if hostAttrs.isNixos
-        then []
-        else
-          inputs.nixpkgs.lib.mapAttrsToList (map (user: {
-              name = "${user}@${hostname}";
-              value = inputs.home-manager.lib.homeManagerConfiguration {
-                pkgs = inputs.nixpkgs.legacyPackages.${hostAttrs.system};
+        inputs.nixpkgs.lib.mapAttrsToList (map (user: {
+            name = "${user}@${hostname}";
+            value = inputs.home-manager.lib.homeManagerConfiguration {
+              pkgs = inputs.nixpkgs.legacyPackages.${hostAttrs.system};
 
-                modules = [
-                  (import ../user/${user}/home/mkHome.nix {
-                    inherit user;
-                    stateVersion = hostAttrs.stateVersion;
-                    isNixos = false;
-                  })
-                  ../user/${user}/home
-                ];
-              };
-            })
-            hostAttrs.users)
+              modules = [
+                (import ../user/${user}/home/mkHome.nix {
+                  inherit user;
+                  stateVersion = hostAttrs.stateVersion;
+                  isNixos = false;
+                })
+                ../user/${user}/home
+              ];
+            };
+          })
+          hostAttrs.users)
     )
-    hosts
+    (inputs.nixpkgs.lib.filterAttrs (hostname: hostAttrs: !hostAttrs.isNixos) hosts)
   );
 }
