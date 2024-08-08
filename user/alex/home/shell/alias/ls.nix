1db350ceeb7831fdb34b90043742793bda4bd24f
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-08 19:51:24 +0200

    better `ll` alias

--- user/alex/home/shell/alias/ls.nix
+++ user/alex/home/shell/alias/ls.nix
@@ -1,6 +1,6 @@
 {
   l = "eza --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
-  ll = "ls -lah";
+  ll = "eza --long --all --group --octal-permissions --headers --no-git --group-directories-first";
 
   lr = "eza --git-ignore --ignore-glob='.git' --recurse --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
   lt = "eza --git-ignore --ignore-glob='.git' --tree --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
