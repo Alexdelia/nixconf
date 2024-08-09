fab2f5b05106bb3c253f31f5698787328d3a1c57
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-09 19:46:12 +0200

    fix: `ll`

--- user/alex/home/shell/alias/ls.nix
+++ user/alex/home/shell/alias/ls.nix
@@ -1,6 +1,6 @@
 {
   l = "eza --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
-  ll = "eza --long --all --group --octal-permissions --headers --no-git --group-directories-first";
+  ll = "eza --long --all --group --octal-permissions --header --no-git --group-directories-first";
 
   lr = "eza --git-ignore --ignore-glob='.git' --recurse --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
   lt = "eza --git-ignore --ignore-glob='.git' --tree --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
