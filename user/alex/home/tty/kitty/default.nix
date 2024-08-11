93137ced2acc0dca255d22dd759d091c3bed1c65
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-11 14:59:48 +0200

    config kitty

--- user/alex/home/tty/kitty/default.nix
+++ user/alex/home/tty/kitty/default.nix
@@ -12,11 +12,36 @@
         size = 16;
       };
 
+      keybindings = {
+        "kitty_mod+f1" = "discard_event";
+      };
+
       settings =
         {
           enable_audio_bell = false;
 
           allow_remote_control = true;
+
+          adjust_line_height = 0;
+          adjust_column_width = 0;
+
+          adjust_baseline = 0;
+
+          /*
+          remember_window_size = false;
+          initial_window_width = 900;
+          initial_window_height = 900;
+          */
+
+          window_border_width = "0.5";
+          draw_minimal_borders = true;
+          window_margin_width = 0;
+          single_window_margin_width = -1;
+          window_padding_width = 0;
+
+          placement_strategy = "center";
+
+          hide_window_decorations = false;
         }
         // (import ./scheme.nix {inherit scheme;});
     };
