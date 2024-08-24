e2b0e3d4b2cf3f06de52f1c46e94d0d143c2ab4b
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-25 00:13:35 +0200

    format: `alejandra .`

--- system/de/gnome/default.nix
+++ system/de/gnome/default.nix
@@ -1,4 +1,8 @@
-{pkgs, lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services.xserver = {
     enable = true;
 
@@ -27,58 +31,60 @@
       atomix # puzzle game
     ]);
 
-	programs.dconf = {
-		enable = true;
+  programs.dconf = {
+    enable = true;
 
-		profiles.user.databases = [{
-        	lockAll = true; # prevents overriding
+    profiles.user.databases = [
+      {
+        lockAll = true; # prevents overriding
 
-        	settings = {
-				"org/gnome/desktop/wm/preferences" = {
-					num-workspaces = lib.gvariant.mkInt32 10;
-				};
-				"org/gnome/shell/app-switcher" = {
-					current-workspace-only = true;
-				};
+        settings = {
+          "org/gnome/desktop/wm/preferences" = {
+            num-workspaces = lib.gvariant.mkInt32 10;
+          };
+          "org/gnome/shell/app-switcher" = {
+            current-workspace-only = true;
+          };
 
-				"org/gnome/desktop/search-providers" = {
-					disabled = [
-						"org.gnome.Nautilus.desktop"
-						"org.gnome.Calendar.desktop"
-						"org.gnome.clocks.desktop"
-						"org.gnome.seahorse.Application.desktop"
-						"org.gnome.Contacts.desktop"
-					];
-				};
+          "org/gnome/desktop/search-providers" = {
+            disabled = [
+              "org.gnome.Nautilus.desktop"
+              "org.gnome.Calendar.desktop"
+              "org.gnome.clocks.desktop"
+              "org.gnome.seahorse.Application.desktop"
+              "org.gnome.Contacts.desktop"
+            ];
+          };
 
-				## shortcut
-				"org/gnome/settings-daemon/plugins/media-keys" = {
-					www = ["<Super>b"];
-					calculator = ["<Super>k"];
-				};
-				"org/gnome/settings-daemon/plugins/media-keys" = {
-					custom-keybindings = [
-						"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-					];
-				};
-				"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-					binding = "<Super>c";
-					command = "kitty";
-					name = "kitty";
-				};
-				"org/gnome/desktop/wm/keybindings" = {
-					"switch-to-workspace-1" = ["<Super>1"];
-					"switch-to-workspace-2" = ["<Super>2"];
-					"switch-to-workspace-3" = ["<Super>3"];
-					"switch-to-workspace-4" = ["<Super>4"];
-					"switch-to-workspace-5" = ["<Super>5"];
-					"switch-to-workspace-6" = ["<Super>6"];
-					"switch-to-workspace-7" = ["<Super>7"];
-					"switch-to-workspace-8" = ["<Super>8"];
-					"switch-to-workspace-9" = ["<Super>9"];
-					"switch-to-workspace-10" = ["<Super>0"];
-				};
-			};
-		}];
-	};
+          ## shortcut
+          "org/gnome/settings-daemon/plugins/media-keys" = {
+            www = ["<Super>b"];
+            calculator = ["<Super>k"];
+          };
+          "org/gnome/settings-daemon/plugins/media-keys" = {
+            custom-keybindings = [
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+            ];
+          };
+          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+            binding = "<Super>c";
+            command = "kitty";
+            name = "kitty";
+          };
+          "org/gnome/desktop/wm/keybindings" = {
+            "switch-to-workspace-1" = ["<Super>1"];
+            "switch-to-workspace-2" = ["<Super>2"];
+            "switch-to-workspace-3" = ["<Super>3"];
+            "switch-to-workspace-4" = ["<Super>4"];
+            "switch-to-workspace-5" = ["<Super>5"];
+            "switch-to-workspace-6" = ["<Super>6"];
+            "switch-to-workspace-7" = ["<Super>7"];
+            "switch-to-workspace-8" = ["<Super>8"];
+            "switch-to-workspace-9" = ["<Super>9"];
+            "switch-to-workspace-10" = ["<Super>0"];
+          };
+        };
+      }
+    ];
+  };
 }
