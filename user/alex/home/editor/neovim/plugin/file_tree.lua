8067814acd6acf56108b2fe51083a45343b1fba4
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-24 12:08:46 +0200

    `nvim-tree` custom mapping

--- user/alex/home/editor/neovim/plugin/file_tree.lua
+++ user/alex/home/editor/neovim/plugin/file_tree.lua
@@ -3,9 +3,26 @@ vim.g.loaded_netrwPlugin = 1
 
 local nvim_tree = require("nvim-tree")
 
+local function nvim_tree_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+	vim.keymap.set('n', '<Right>', api.node.open.edit, opts('Open'))
+	vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+	vim.keymap.set('n', '<Left>', api.node.navigate.parent_close, opts('Close Folder'))
+	vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Folder'))
+end
+
 -- `:help nvim-tree.OPTION_NAME`
 
 nvim_tree.setup {
+	on_attach = nvim_tree_on_attach,
+
     disable_netrw = true,
     hijack_netrw = true,
 
