ef98e427d607784109f4159cc0618657f23dce34
Alexdelia <alexandre.delille.57@gmail.com>
2024-08-25 12:04:44 +0200

    gtk

---
+++ user/alex/home/de/gtk/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, ...}: {
+  gtk = {
+  	enable = true;
+
+	theme.name = "Sweet-Dark";
+	theme.package = pkgs.sweet;
+  };
+
+  stylix.targets.gtk.enable = false;
+}
--- user/alex/home/default.nix
+++ user/alex/home/default.nix
@@ -12,5 +12,6 @@
     ./tty
     ./editor
     ./browser
+	./de
   ];
 }
