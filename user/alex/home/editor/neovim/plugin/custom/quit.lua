86078df10406707ddbf92b8ca7fd79eb9ac372ba
Alexdelia <alexandre.delille.57@gmail.com>
2024-09-01 13:29:11 +0200

    even better quit

--- user/alex/home/editor/neovim/plugin/custom/quit.lua
+++ user/alex/home/editor/neovim/plugin/custom/quit.lua
@@ -1,5 +1,9 @@
 local function quit()
-	if vim.fn.len(vim.fn.getbufinfo({buflisted = 1})) == 1 then
+	local open_buffer = vim.fn.len(vim.fn.getbufinfo({buflisted = 1}))
+
+	if open_buffer == 0 then
+		vim.cmd("q")
+	elseif open_buffer == 1 then
 		vim.cmd("w | q | bd #")
 	else
 		vim.cmd("w | bp | bd #")
