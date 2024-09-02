d349709c41cb73a866fabec6d44452aafb82b1f7
Alexdelia <alexandre.delille.57@gmail.com>
2024-09-02 15:45:15 +0200

    rust

--- user/alex/home/editor/neovim/plugin/icons.lua
+++ user/alex/home/editor/neovim/plugin/icons.lua
@@ -4,7 +4,8 @@ local icons = {
     default = {icon = "", color = "#6d8086", name = "Default"},
     license = {icon = "", color = "#333333", name = "License"},
     lock = {icon = "", color = "#444444", name = "Lock"},
-    sh = {icon = "", color = "#ff7043", name = "Shell"}
+    sh = {icon = "", color = "#ff7043", name = "Shell"},
+	rust = {icon = "", color = "#8a3510", name = "Rust"}
 }
 
 devicons.setup({
@@ -13,7 +14,7 @@ devicons.setup({
     strict = true,
 
     override_by_filename = {LICENSE = icons.license, license = icons.license},
-    override_by_extension = {lock = icons.lock, sh = icons.sh}
+    override_by_extension = {lock = icons.lock, sh = icons.sh, rs = icons.rust},
 })
 
 devicons.set_icon({LICENSE = icons.license})
