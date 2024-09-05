d78afbd3dccfe8763bd46282ca5d2d8a3f23c9ad
Alexdelia <alexandre.delille.57@gmail.com>
2024-09-05 23:13:51 +0200

    default to `nix shell`

--- user/alex/home/script/ns.nix
+++ user/alex/home/script/ns.nix
@@ -6,13 +6,12 @@
   */
   ''
     if [[ $# -eq 0 ]]; then
-      printf "usage: \033[1m$0 \033[35m<pkg1> <pkg2> ...\033[0m\n"
-      exit 64 # sysexits.h `EX_USAGE` https://github.com/openbsd/src/blob/master/include/sysexits.h#L101
+    	nix shell
     fi
 
     pkgs=""
     for pkg in "$@"; do
-      pkgs+="nixpkgs#$pkg "
+    	pkgs+="nixpkgs#$pkg "
     done
 
     nix shell $pkgs
