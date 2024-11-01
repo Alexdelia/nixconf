cd07cd0014de0634b58cd970a6e9b3d086224e15
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-01 20:43:41 +0100

    simplified

--- common/option/dp.nix
+++ common/option/dp.nix
@@ -6,22 +6,11 @@
   options.dp = lib.mkOption {
     description = "preferred packages";
 
-    type = lib.types.attrsOf (lib.types.submodule ({name, ...}: {
-      options = {
-        bin = lib.mkOption {type = lib.types.path;};
-        name = lib.mkOption {type = lib.types.str;};
-      };
-    }));
+    type = lib.types.attrsOf lib.types.path;
 
     default = {
-      term = {
-        bin = "${pkgs.alacritty}/bin/alacritty";
-        name = "alacritty";
-      };
-      editor = {
-        bin = "${pkgs.vim}/bin/vim";
-        name = "vim";
-      };
+      term = "${pkgs.alacritty}/bin/alacritty";
+      editor = "${pkgs.vim}/bin/vim";
     };
   };
 }
