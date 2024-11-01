0b94cfcf14cf7dfdd72e87f0117ca0f44fb5ed8d
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-01 20:51:03 +0100

    removing duplicate

--- common/option/dp.nix
+++ common/option/dp.nix
@@ -4,12 +4,13 @@
   ...
 }: {
   options.dp = lib.mkOption {
-    description = "preferred packages";
+    description = "preferred packages (default packages|program|path)";
 
     type = lib.types.attrsOf lib.types.path;
 
     default = {
       term = "${pkgs.alacritty}/bin/alacritty";
+      shell = "${pkgs.bash}/bin/bash";
       editor = "${pkgs.vim}/bin/vim";
     };
   };
