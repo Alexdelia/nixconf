b55605466fbabacb34620d6713a6372512fec5d7
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-02 20:20:30 +0100

    keybind

--- user/alex/home/de/hyprland/keybind.nix
+++ user/alex/home/de/hyprland/keybind.nix
@@ -1,12 +1,7 @@
-{config, ...}:
-#let
-#bind = (mod, key, dispatcher, {
-{
+{config, ...}: {
   wayland.windowManager.hyprland.settings.bind = [
     "$mod, C, exec, ${config.dp.term}"
     "$mod, B, exec, ${config.dp.browser}"
-    # "$mod, C, exec, alacritty"
-    # "$mod, B, exec, brave"
 
     "$mod SHIFT, Q, killactive,"
     "$mod SHIFT, M, exit,"
