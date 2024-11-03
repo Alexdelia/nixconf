07caeafa4d4ed8428b5e921b9bcd22c3a0206dd3
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-03 18:47:56 +0100

    `anyrun` `css`

--- user/alex/home/de/runner/anyrun.nix
+++ user/alex/home/de/runner/anyrun.nix
@@ -12,7 +12,7 @@
     enable = true;
 
     config = let
-      width = 1.0 / 4.0;
+      width = 1.0 / 2.0;
     in {
       hideIcons = false;
       ignoreExclusiveZones = false;
@@ -27,7 +27,7 @@
 
       layer = "overlay";
 
-      closeOnClick = false;
+      closeOnClick = true;
 
       plugins = with inputs.anyrun.packages.${pkgs.system}; [
         applications
@@ -44,7 +44,7 @@
         ''
           Config(
             desktop_actions: false,
-            max_entries: 8,
+            max_entries: 4,
             terminal: Some("${config.dp.term}"),
           )
         '';
@@ -52,19 +52,61 @@
 
     # Inline comments are supported for language injection into
     # multi-line strings with Treesitter! (Depends on your editor)
-    # extraCss = /*css */ ''
-    /*
-    .some_class {
-    */
-    /*
-    background: red;
-    */
-    /*
-    }
-    */
-    /*
-    '';
-    */
+    extraCss =
+      /*
+      css
+      */
+      ''
+        * {
+        	all: unset;
+
+        	font-size: 3rem;
+        	font-family: "SauceCodeProNerdFont";
+        	/* font-weight: bold; */
+
+        	/* transition: 500ms; */
+        }
+
+        #window,
+        #match,
+        #entry,
+        #plugin,
+        #main {
+        	background: transparent;
+        }
+
+        box#main {
+        	background: rgba(0, 0, 0, 1);
+        	border-radius: 1.5rem;
+        	padding: 1.5rem;
+        }
+
+        #entry {
+        	background: rgba(0, 0, 255, 0.1);
+        	border-radius: 0.5rem;
+        	padding: 1rem;
+        }
+
+        #match.activatable {
+        	border-radius: 0.5rem;
+        	margin: 0.5rem 0;
+        	padding: 0.5rem;
+        	background: rgba(255, 255, 255, 0.05);
+        }
+        #match.activatable:first-child {
+        	margin-top: 1.5rem;
+        }
+        #match.activatable:last-child {
+        	margin-bottom: 0;
+        }
+
+        #match:hover {
+        	background: rgba(255, 255, 255, 0.1);
+        }
+        #match:selected {
+        	background: rgba(255, 0, 0, 0.2);
+        }
+      '';
   };
 
   # nix.settings = {
