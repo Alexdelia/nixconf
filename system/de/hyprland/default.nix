94eaebc440afed65a2bf762e616bc48ff19fc903
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-03 16:17:29 +0100

    auto login

--- system/de/hyprland/default.nix
+++ system/de/hyprland/default.nix
@@ -1,7 +1,15 @@
-{
+{users, ...}: {
   programs.hyprland = {
     enable = true;
 
     xwayland.enable = true;
   };
+
+  services.getty.autologinUser = builtins.elemAt users 0;
+
+  environment.loginShellInit = ''
+    if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+      exec dbus-launch hyprland
+    fi
+  '';
 }
