a56edf5937bd5a580461fdc2f1ac89b11864708e
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-12 17:14:29 +0100

    default browser mime

--- user/alex/home/pkg/mime.nix
+++ user/alex/home/pkg/mime.nix
@@ -3,6 +3,10 @@
     enable = true;
 
     defaultApplications = {
+      "x-scheme-handler/http" = config.dm.browser;
+      "x-scheme-handler/https" = config.dm.browser;
+      "text/html" = config.dm.browser;
+
       "audio/mpeg" = config.dm.browser;
     };
   };
