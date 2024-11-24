8bd55c7b860ab8982fb4a122dbe3c891fcf7e9b4
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-24 16:42:55 +0100

    prepare for template update

--- user/alex/home/sops/wakatime.nix
+++ user/alex/home/sops/wakatime.nix
@@ -1,18 +1,17 @@
-# {
-# config,
-# username,
-# ...
-# }:
 {
+  config,
+  # username,
+  ...
+}: {
   sops = {
     secrets."wakatime/api_key" = {
       # owner = username;
       # path = "/home/${username}/test";
     };
-    # };
-    # templates."test.toml".content = ''
-    # [settings]
-    # api_key="${config.sops.placeholder.wakatime.api_key}"
-    # '';
+
+    templates."test.toml".content = ''
+      [settings]
+      api_key="${config.sops.placeholder.wakatime.api_key}"
+    '';
   };
 }
