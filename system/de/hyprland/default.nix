b16a02597795dceb44246d26f3c1a57d7823197d
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-08 16:40:02 +0100

    protection against default config

--- system/de/hyprland/default.nix
+++ system/de/hyprland/default.nix
@@ -1,14 +1,16 @@
-{users, ...}: {
+{users, ...}: let
+  mainUser = builtins.elemAt users 0;
+in {
   programs.hyprland = {
     enable = true;
 
     xwayland.enable = true;
   };
 
-  services.getty.autologinUser = builtins.elemAt users 0;
+  services.getty.autologinUser = mainUser;
 
   environment.loginShellInit = ''
-    if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+    if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ] && [ -L "/home/${mainUser}/.config/hypr/hyprland.conf" ]; then
       exec dbus-launch Hyprland || echo 'oh no!'
     fi
   '';
