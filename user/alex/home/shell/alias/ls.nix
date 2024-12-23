ee2683f8e37faa13acb97641a6c6ab400c92a337
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-23 22:50:24 +0100

    remove incorrect use of option

--- user/alex/home/shell/alias/ls.nix
+++ user/alex/home/shell/alias/ls.nix
@@ -1,7 +1,7 @@
 {
-  l = "eza --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
+  l = "eza --long --all --no-permissions --no-filesize --no-user --no-time --git";
   ll = "eza --long --all --group --octal-permissions --header --no-git --group-directories-first";
 
-  lr = "eza --git-ignore --ignore-glob='.git' --recurse --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
-  lt = "eza --git-ignore --ignore-glob='.git' --tree --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git --icons";
+  lr = "eza --git-ignore --ignore-glob='.git' --recurse --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git";
+  lt = "eza --git-ignore --ignore-glob='.git' --tree --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git";
 }
