3bf7bfe45b82060b1f607bedd2a799a7f1635ddd
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-24 22:13:26 +0100

    `info-hub` script

--- user/alex/home/de/eww/default.nix
+++ user/alex/home/de/eww/default.nix
@@ -1,5 +1,5 @@
-{pkgs, ...}: {
-  dp.infoHub =
+{pkgs, ...}: let
+  infoHub =
     pkgs.writers.writeBashBin "info-hub" {}
     /*
     bash
@@ -8,6 +8,12 @@
       # eww open --toggle info_hub
       eww -c ${./src} open --toggle info_hub
     '';
+in {
+  home.packages = [
+    infoHub
+  ];
+
+  dp.infoHub = infoHub;
 
   programs.eww = {
     enable = true;
