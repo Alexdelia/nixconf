ce38b218288a34bbf5e8a177629464098100b0c8
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-25 15:55:21 +0100

    progress

--- user/alex/home/music/mpd/mpdris2.nix
+++ user/alex/home/music/mpd/mpdris2.nix
@@ -1,3 +1,32 @@
-{config, ...}: {
+{
+  config,
+  # pkgs,
+  # lib,
+  ...
+}: {
+  /*
+  home.packages = with pkgs; [
+    mpdris2
+  ];
+  */
+
   services.mpdris2.enable = config.hostOption.entertainment.music;
+
+  /*
+  systemd.user.services.mpdris2 = {
+    Service = {
+      Type = lib.mkForce "dbus";
+
+      # ExecStart = lib.mkForce "${config.services.mpdris2.package}/bin/mpDris2 --debug";
+
+      ExecStartPost = "${pkgs.dbus}/bin/dbus-send --session --dest=org.mpris.MediaPlayer2.mpd --type=method_call --print-reply /org/mpris/MediaPlayer2 org.freedesktop.DBus.Peer.Ping";
+    };
+
+    Unit.After = lib.mkForce [
+      "mpd.service"
+      "pulseaudio.service"
+      "graphical-session.target"
+    ];
+  };
+  */
 }
