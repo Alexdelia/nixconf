76dc490d1354f1cc4389ca4288add1305028c332
Alexdelia <alexandre.delille.57@gmail.com>
2024-12-28 21:50:21 +0100

    input

--- user/alex/home/de/sway/default.nix
+++ user/alex/home/de/sway/default.nix
@@ -18,6 +18,13 @@
           mode = "1920x1080@60.000Hz";
         };
       };
+
+      input = {
+        "1267:12608:ELAN0001:00_04F3:3140_Touchpad" = {
+          tap = "enabled";
+          natural_scroll = "enabled";
+        };
+      };
     };
   };
 }
