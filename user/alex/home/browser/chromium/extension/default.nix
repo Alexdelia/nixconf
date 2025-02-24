558a55a14d18e2c73966de697cf2f05fd01ba0fb
Alexdelia <alexandre.delille.57@gmail.com>
2025-02-24 12:07:28 +0100

    testing language tool extension

--- user/alex/home/browser/chromium/extension/default.nix
+++ user/alex/home/browser/chromium/extension/default.nix
@@ -1,24 +1,20 @@
 {config, ...}: {
   programs.chromium.extensions =
     [
-      (import
-        ./dark_reader.nix)
-      (import
-        ./material_icons_for_github.nix)
-      (import
-        ./tampermonkey.nix)
-      (import
-        ./improved_intra_42.nix)
+      (import ./dark_reader.nix)
+
+      (import ./material_icons_for_github.nix)
+      (import ./improved_intra_42.nix)
+
+      (import ./language_tool.nix)
+      # (import ./tampermonkey.nix)
     ]
     ++ (
       if config.hostOption.entertainment.gaming
       then [
-        (import
-          ./protondb.nix)
-        (import
-          ./augmented_steam.nix)
-        (import
-          ./steamdb.nix)
+        (import ./protondb.nix)
+        (import ./augmented_steam.nix)
+        (import ./steamdb.nix)
       ]
       else []
     );
