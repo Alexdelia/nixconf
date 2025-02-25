b4ba17cc179af4c7766125552f04aa4aa3689089
Alexdelia <alexandre.delille.57@gmail.com>
2025-02-25 19:19:27 +0100

    switch to `osu` on `nixos` config instead of `home`

--- common/option/host-option/entertainment/gaming/osu.nix
+++ common/option/host-option/entertainment/gaming/osu.nix
@@ -5,7 +5,12 @@
   ...
 }: {
   config = lib.mkIf config.hostOption.entertainment.gaming {
+    /*
     home.packages = with pkgs.unstable; [
+      osu-lazer-bin
+    ];
+    */
+    environment.systemPackages = with pkgs.unstable; [
       osu-lazer-bin
     ];
   };
