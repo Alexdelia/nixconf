b0566bfb9538df5571b6a8d7fd2b49c8554fead2
Alexdelia <alexandre.delille.57@gmail.com>
2025-03-06 13:34:41 +0100

    improve ls alias

--- user/alex/home/shell/alias/default.nix
+++ user/alex/home/shell/alias/default.nix
@@ -5,6 +5,6 @@
 (import ./short.nix {inherit config pkgs;})
 // import ./alternative.nix
 // import ./oneliner.nix
-// import ./ls.nix
+// (import ./ls.nix {inherit pkgs;})
 // import ./cd.nix
 // import ./git.nix
--- user/alex/home/shell/alias/ls.nix
+++ user/alex/home/shell/alias/ls.nix
@@ -1,7 +1,19 @@
-{
-  l = "eza --long --all --no-permissions --no-filesize --no-user --no-time --git";
-  ll = "eza --long --all --group --octal-permissions --header --no-git --group-directories-first";
+{pkgs, ...}: let
+  cmd = "${pkgs.eza}/bin/eza";
 
-  lr = "eza --git-ignore --ignore-glob='.git' --recurse --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git";
-  lt = "eza --git-ignore --ignore-glob='.git' --tree --group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git";
+  list_node = "--group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git";
+
+  list_recursive = "--recurse ${list_node}";
+  list_tree = "--tree ${list_node}";
+
+  ignore = "--git-ignore --ignore-glob='.git'";
+in {
+  l = "${cmd} --long --all --no-permissions --no-filesize --no-user --no-time --git";
+  ll = "${cmd} --long --all --group --octal-permissions --header --no-git --group-directories-first";
+
+  lr = "${cmd} ${list_recursive} ${ignore}";
+  lt = "${cmd} ${list_tree} ${ignore}";
+
+  lrf = "${cmd} ${list_recursive}";
+  ltf = "${cmd} ${list_tree}";
 }
