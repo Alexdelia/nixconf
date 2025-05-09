f8d2e97d6f9cecdab46e6f9032cf6e9db54e6a0f
Alexdelia <alexandre.delille.57@gmail.com>
2025-05-09 11:36:55 +0200

    first iteration of `git commit version`

---
+++ user/alex/home/pkg/git/script/gcv.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: (
+  pkgs.writers.writeBashBin
+  "gcv" {}
+  /*
+  bash
+  */
+  ''
+    if [[ $# -neq 2 ]]; then
+    	printf "usage: \033[1m$0 \033[35m<semver> <commit message>\033[0m\n"
+    	return 64 # sysexits.h `EX_USAGE` https://github.com/openbsd/src/blob/master/include/sysexits.h#L101
+    fi
+
+    git commit -m 'ver: `'"$1"'` - '"$2" --no-verify
+    git tag -a "v$1" -m 'version `'"$1"'` - '"$2"
+    git push --no-verify
+    git push origin tag "v$1" --no-verify
+  ''
+)
