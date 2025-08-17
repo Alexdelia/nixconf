613325024094987f1f2c9c75cb6427c2d8830b08
Alexdelia <alexandre.delille.57@gmail.com>
2025-08-17 23:34:50 +0200

    switch `LC_TIME` to have monday on first day of week and simplify locale

--- system/locale.nix
+++ system/locale.nix
@@ -3,8 +3,8 @@
     defaultLocale = "C.UTF-8";
 
     extraLocaleSettings = {
-      # 31/01/1970 24:59:59
-      LC_TIME = "en_SG.UTF-8";
+      # 31/01/1970 23:59:59
+      LC_TIME = "en_NZ.UTF-8";
       # 1 234.56
       LC_NUMERIC = "mfe_MU";
       # € 1234,56
@@ -16,7 +16,7 @@
       LC_TELEPHONE = "fr_FR.UTF-8";
       LC_IDENTIFICATION = "fr_FR.UTF-8";
 
-      LC_NAME = "en_US.UTF-8";
+      LC_NAME = "en_NZ.UTF-8";
 
       LC_PAPER = "nl_NL.UTF-8";
     };
