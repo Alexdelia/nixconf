5f0ad1770797fd081c7f94c12038b638e8468f39
Alexdelia <alexandre.delille.57@gmail.com>
2025-11-22 19:15:24 +0100

    feat: more `bat` alias

--- user/alex/home/shell/alias/alternative.nix
+++ user/alex/home/shell/alias/alternative.nix
@@ -1,9 +1,14 @@
 {pkgs}: {
   j = "z";
   cp = "xcp -r";
+
   cat = "bat -p";
-  rat = "bat -pA";
   ca = "${pkgs.uutils-coreutils-noprefix}/bin/cat";
-  man = "batman";
+  rat = "bat -pA";
+  bar = "bat -pA";
+  man = "bat -l=man";
+  bam = "bat -l=man";
+  baj = "bat -l=json";
+
   jq = "jaq";
 }
