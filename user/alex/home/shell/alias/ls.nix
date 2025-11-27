bdfe32146d1d54f9e3db065661bdec7d43187534
Alexdelia <alexandre.delille.57@gmail.com>
2025-11-27 22:11:23 +0100

    feat: icons on `ls` `alias`

--- user/alex/home/shell/alias/ls.nix
+++ user/alex/home/shell/alias/ls.nix
@@ -1,15 +1,15 @@
 {pkgs, ...}: let
   cmd = "${pkgs.eza}/bin/eza";
 
-  list_node = "--group-directories-first --long --all --no-permissions --no-filesize --no-user --no-time --git";
+  list_node = "--group-directories-first --long --all --icons=always --no-permissions --no-filesize --no-user --no-time --git";
 
   list_recursive = "--recurse ${list_node}";
   list_tree = "--tree ${list_node}";
 
   ignore = "--git-ignore --ignore-glob='.git'";
 in {
-  l = "${cmd} --long --all --no-permissions --no-filesize --no-user --no-time --git";
-  ll = "${cmd} --long --all --group --octal-permissions --header --no-git --group-directories-first";
+  l = "${cmd} --long --all --icons=always --no-permissions --no-filesize --no-user --no-time --git";
+  ll = "${cmd} --long --all --icons=always --group --octal-permissions --header --no-git --group-directories-first";
 
   lr = "${cmd} ${list_recursive} ${ignore}";
   lt = "${cmd} ${list_tree} ${ignore}";
