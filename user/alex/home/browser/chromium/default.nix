723d25b122de9a5db5e15ead816e2ae7e2c84022
Alexdelia <alexandre.delille.57@gmail.com>
2026-01-28 09:45:23 +0100

    feat: use `brave` again

--- user/alex/home/browser/chromium/default.nix
+++ user/alex/home/browser/chromium/default.nix
@@ -1,26 +1,5 @@
-{
-  config,
-  pkgs,
-  ...
-}: let
-  package =
-    if !config.targets.genericLinux.enable
-    then pkgs.brave
-    else
-      pkgs.chromium.overrideAttrs (old: {
-        buildCommand = let
-          oldStr = ''
-            if [ -x "/run/wrappers/bin/${old.passthru.sandboxExecutableName}" ]
-            then
-              export CHROME_DEVEL_SANDBOX="/run/wrappers/bin/${old.passthru.sandboxExecutableName}"
-            else
-              export CHROME_DEVEL_SANDBOX="$sandbox/bin/${old.passthru.sandboxExecutableName}"
-            fi
-          '';
-          newStr = ''export CHROME_DEVEL_SANDBOX=/opt/google/chrome/chrome-sandbox'';
-        in
-          builtins.replaceStrings [oldStr] [newStr] old.buildCommand;
-      });
+{pkgs, ...}: let
+  package = pkgs.brave;
 in {
   imports = [
     ./extension
--- user/alex/home/browser/default.nix
+++ user/alex/home/browser/default.nix
@@ -6,11 +6,8 @@
   dp.browser =
     if !config.targets.genericLinux.enable
     then "${pkgs.brave}/bin/brave"
-    else "${pkgs.chromium}/bin/chromium";
-  dm.browser =
-    if !config.targets.genericLinux.enable
-    then "brave-browser.desktop"
-    else "chromium-browser.desktop";
+    else "/usr/bin/brave-browser"; # manual install for sandboxed capabilities
+  dm.browser = "brave-browser.desktop";
 
   imports = [
     ./chromium
