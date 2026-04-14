918c2b1a7f9b11197c4d1cc1305891e29f401e4f
Alexdelia <alexandre.delille.57@gmail.com>
2026-04-14 16:39:00 +0200

    fix: correct icon override

--- user/alex/home/editor/neovim/plugin/icons.lua
+++ user/alex/home/editor/neovim/plugin/icons.lua
@@ -5,18 +5,21 @@ local icons = {
 	license = { icon = "", color = "#333333", name = "License" },
 	lock = { icon = "", color = "#444444", name = "Lock" },
 	sh = { icon = "", color = "#ff7043", name = "Shell" },
-	rust = { icon = "", color = "#8a3510", name = "Rust" }
+	rs = { icon = "", color = "#8a3510", name = "Rs" }
 }
 
 devicons.setup({
-	-- color_icons = true;
-	-- default = false;
-	strict = true,
+	strict = false,
 
-	override_by_filename = { LICENSE = icons.license, license = icons.license },
-	override_by_extension = { lock = icons.lock, sh = icons.sh, rs = icons.rust }
+	override_by_filename = {
+		LICENSE = icons.license,
+		license = icons.license,
+	},
+	override_by_extension = {
+		lock = icons.lock,
+		sh = icons.sh,
+		rs = icons.rs,
+	}
 })
-
-devicons.set_icon({ LICENSE = icons.license })
 
 devicons.set_default_icon(icons.default.icon, icons.default.color, 65)
