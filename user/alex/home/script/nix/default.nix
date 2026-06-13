0c1dc42715f6146a560f66a92bbc5ff5c4642cd7
Alexdelia <alexandre.delille.57@gmail.com>
2026-06-13 11:49:43 +0200

    feat: `nix "unstable" shell` & `nix "unstable" shell "un-free"`

--- user/alex/home/script/nix/default.nix
+++ user/alex/home/script/nix/default.nix
@@ -2,6 +2,8 @@
   home.packages = [
     (import ./nr.nix {inherit pkgs;})
     (import ./ns.nix {inherit pkgs;})
+    (import ./nus.nix {inherit pkgs;})
     (import ./nsu.nix {inherit pkgs;})
+    (import ./nusu.nix {inherit pkgs;})
   ];
 }
