3b2cf64dd1872e71982845ffbc3787fd853c444d
Alexdelia <alexandre.delille.57@gmail.com>
2026-07-11 19:43:25 +0200

    feat: `OpenTabletDriver` gated on tablet actually being plugged

--- host/jiruo/opentabletdriver.nix
+++ host/jiruo/opentabletdriver.nix
@@ -1,3 +1,9 @@
-{
+{lib, ...}: {
   hardware.opentabletdriver.enable = true;
+
+  systemd.user.services.opentabletdriver.wantedBy = lib.mkForce [];
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="28bd", ATTR{idProduct}=="0075", TAG+="systemd", ENV{SYSTEMD_USER_WANTS}+="opentabletdriver.service"
+  '';
 }
