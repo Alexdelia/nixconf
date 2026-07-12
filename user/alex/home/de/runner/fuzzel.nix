d647b355e6ebd70c9faa1a0049e6e860869fcc43
Alexdelia <alexandre.delille.57@gmail.com>
2026-07-12 08:35:45 +0200

    feat: improve `fuzzel` config

--- user/alex/home/de/runner/fuzzel.nix
+++ user/alex/home/de/runner/fuzzel.nix
@@ -16,10 +16,13 @@ in {
         terminal = config.dp.term;
         layer = "overlay";
 
-        font = "SauceCodeProNerdFont:size=18";
-        prompt = "";
+        # font = "SauceCodeProNerdFont:size=36";
+        font = "RobotoMono Nerd Font Mono:size=36";
+
+        prompt = ''"  "'';
 
         anchor = "top";
+
         y-margin = 300;
         width = 40;
         lines = 8;
@@ -30,19 +33,20 @@ in {
       };
 
       border = {
-        width = 3;
-        radius = 15;
+        width = 0;
+        radius = 20;
+        selection-radius = 8;
       };
 
       colors = {
         background = "${s.base00}f5";
         text = "${s.base05}ff";
-        prompt = "${s.base05}ff";
+        prompt = "${s.base05}30";
         match = "${s.base0B}ff";
         selection = "${s.base0B}40";
         selection-text = "${s.base05}ff";
         selection-match = "${s.base0B}ff";
-        border = "${s.base0D}80";
+        # border = "${s.base0D}80";
       };
     };
   };
