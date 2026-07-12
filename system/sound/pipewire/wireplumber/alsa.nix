10f005c7f3d43fc7c1e849b36d20db4c61029fdb
Alexdelia <alexandre.delille.57@gmail.com>
2026-07-12 07:33:50 +0200

    feat: hydrate alsa rules

--- system/sound/pipewire/wireplumber/alsa.nix
+++ system/sound/pipewire/wireplumber/alsa.nix
@@ -28,5 +28,33 @@
         };
       };
     }
+    {
+      matches = [
+        {
+          "node.name" = "alsa_output.pci-0000_00_1f.3.analog-stereo";
+        }
+      ];
+      actions = {
+        "update-props" = {
+          "node.nick" = "internal analog";
+          "priority.driver" = 10000;
+          "priority.session" = 10000;
+        };
+      };
+    }
+    {
+      matches = [
+        {
+          "node.name" = "alsa_output.usb-0b0e_Jabra_SOLEMATE_v1.34.0-00.analog-stereo";
+        }
+      ];
+      actions = {
+        "update-props" = {
+          "node.nick" = "jabra usb charge";
+          "priority.driver" = 0;
+          "priority.session" = 0;
+        };
+      };
+    }
   ];
 }
