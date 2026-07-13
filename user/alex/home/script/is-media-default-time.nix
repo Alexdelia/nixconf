6de3560fc045d86e197f4c227e1f579a0c558b18
Alexdelia <alexandre.delille.57@gmail.com>
2026-07-13 18:58:27 +0200

    feat: better condition

--- user/alex/home/script/is-media-default-time.nix
+++ user/alex/home/script/is-media-default-time.nix
@@ -4,10 +4,12 @@ in
   pkgs.writeShellApplication {
     name = "is-media-default-time";
     runtimeInputs = with pkgs; [uutils-coreutils-noprefix];
-    text = ''
-      ${pkgs.lib.optionalString forceIsMediaDefaultTime "exit 0"}
-      d="$(date +%u)"
-      h=$((10#$(date +%H)))
-      [ "$d" -ge 1 ] && [ "$d" -le 5 ] && [ "$h" -lt 17 ]
-    '';
+    text =
+      if forceIsMediaDefaultTime
+      then "exit 0"
+      else ''
+        d="$(date +%u)"
+        h=$((10#$(date +%H)))
+        [ "$d" -ge 1 ] && [ "$d" -le 5 ] && [ "$h" -lt 17 ]
+      '';
   }
