b1a48fe3d8abce3f0681f18bb73fe20a2e523d6f
Alexdelia <alexandre.delille.57@gmail.com>
2026-07-14 16:01:26 +0200

    feat: more shortcut

--- user/alex/home/de/sway/keybind.nix
+++ user/alex/home/de/sway/keybind.nix
@@ -7,13 +7,18 @@
   inherit (config.wayland.windowManager.sway.config) modifier;
 
   notify = "${pkgs.libnotify}/bin/notify-send";
+
+  playerctl = "${pkgs.playerctl}/bin/playerctl";
+
+  wpctl = "${pkgs.wireplumber}/bin/wpctl";
+  baseVolumeChange = 2;
+  highVolumeChange = baseVolumeChange * 4;
+
+  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  baseBrightnessChange = 2;
+  highBrightnessChange = baseBrightnessChange * 4;
 in {
   wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
-    # apps
-    "${modifier}+c" = "exec ${config.dp.term}";
-    "${modifier}+b" = "exec ${config.dp.browser}";
-    "${modifier}+k" = "exec ${config.dp.calculator}";
-
     # widgets
     "${modifier}+d" = "exec ${config.dp.dmenu}";
     "${modifier}+a" = "exec ${config.dp.infoHub or notify + " 'no infoHub'"}";
@@ -29,5 +34,24 @@ in {
 
     # window manager
     "${modifier}+Shift+q" = "kill";
+
+    # media audio
+    "XF86AudioPlay" = "exec ${playerctl} play-pause";
+    "XF86AudioNext" = "exec ${playerctl} next";
+    "XF86AudioPrev" = "exec ${playerctl} previous";
+    "XF86AudioStop" = "exec ${playerctl} stop";
+
+    # volume
+    "XF86AudioRaiseVolume" = "exec ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${toString baseVolumeChange}%+";
+    "XF86AudioLowerVolume" = "exec ${wpctl} set-volume @DEFAULT_AUDIO_SINK@ ${toString baseVolumeChange}%-";
+    "Shift+XF86AudioRaiseVolume" = "exec ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${toString highVolumeChange}%+";
+    "Shift+XF86AudioLowerVolume" = "exec ${wpctl} set-volume @DEFAULT_AUDIO_SINK@ ${toString highVolumeChange}%-";
+    "XF86AudioMute" = "exec ${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
+
+    # brightness
+    "XF86MonBrightnessUp" = "exec ${brightnessctl} set ${toString baseBrightnessChange}%+";
+    "XF86MonBrightnessDown" = "exec ${brightnessctl} set ${toString baseBrightnessChange}%-";
+    "Shift+XF86MonBrightnessUp" = "exec ${brightnessctl} set ${toString highBrightnessChange}%+";
+    "Shift+XF86MonBrightnessDown" = "exec ${brightnessctl} set ${toString highBrightnessChange}%-";
   };
 }
--- user/alex/home/keyboard/swhkd/keybind.nix
+++ user/alex/home/keyboard/swhkd/keybind.nix
@@ -5,25 +5,19 @@
   ...
 }: let
   keybind = let
+    notify = "${pkgs.libnotify}/bin/notify-send";
+
     playerctl = "${pkgs.playerctl}/bin/playerctl";
     mpc = "${pkgs.mpc}/bin/mpc";
-
-    wpctl = "${pkgs.wireplumber}/bin/wpctl";
-    baseVolumeChange = 2;
-    highVolumeChange = baseVolumeChange * 4;
-
-    brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
-    baseBrightnessChange = 2;
-    highBrightnessChange = baseBrightnessChange * 4;
   in {
-    # apps
-    # "super + c" = config.dp.term;
-    # "super + b" = config.dp.browser;
-    # "super + m" = config.dp.music;
-    # "super + k" = config.dp.calculator;
-    # "super + f" = config.dp.fileManager;
+    # app
+    "super + c" = config.dp.term;
+    "super + b" = config.dp.browser;
+    "super + m" = config.dp.music;
+    "super + k" = config.dp.calculator;
+    "super + f" = "${config.dp.fileManager or notify + " 'no fileManager'"}";
 
-    # widgets
+    # widget
     # "super + d" = config.dp.dmenu;
     # "super + a" = config.dp.infoHub;
     # "super + w" = config.dp.powermenu;
@@ -34,26 +28,9 @@
 
     # media audio
     "super + z" = "${playerctl} play-pause";
-    "xf86audioplay" = "${playerctl} play-pause";
     "super + shift + z" = "${mpc} toggle";
     "super + x" = "${playerctl} next";
-    "xf86audionext" = "${playerctl} next";
     "super + shift + x" = "${mpc} next";
-    "xf86audioprev" = "${playerctl} previous";
-    "xf86audiostop" = "${playerctl} stop";
-
-    # volume
-    "volumeup" = "${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${toString baseVolumeChange}%+";
-    "volumedown" = "${wpctl} set-volume @DEFAULT_AUDIO_SINK@ ${toString baseVolumeChange}%-";
-    "shift + volumeup" = "${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${toString highVolumeChange}%+";
-    "shift + volumedown" = "${wpctl} set-volume @DEFAULT_AUDIO_SINK@ ${toString highVolumeChange}%-";
-    "mute" = "${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle";
-
-    # brightness
-    "brightnessup" = "${brightnessctl} set ${toString baseBrightnessChange}%+";
-    "brightnessdown" = "${brightnessctl} set ${toString baseBrightnessChange}%-";
-    "shift + brightnessup" = "${brightnessctl} set ${toString highBrightnessChange}%+";
-    "shift + brightnessdown" = "${brightnessctl} set ${toString highBrightnessChange}%-";
   };
 
   isNonNixos = config.targets.genericLinux.enable;
