05476458d0dcd265d5e7ba20a1c36ea4c1d8d443
Alexdelia <alexandre.delille.57@gmail.com>
2026-07-30 14:22:32 +0200

    switch `en_NZ` to `en_GB`

--- common/locale.nix
+++ common/locale.nix
@@ -3,7 +3,7 @@
 
   formats = {
     # 31/01/1970 23:59:59
-    LC_TIME = "en_NZ.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
     # 1 234.56
     LC_NUMERIC = "mfe_MU";
     # € 1234,56
@@ -15,7 +15,7 @@
     LC_TELEPHONE = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
 
-    LC_NAME = "en_NZ.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
 
     LC_PAPER = "nl_NL.UTF-8";
   };
