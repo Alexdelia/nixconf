21358c139685721c2263e48612dc7d7aefd2ed4f
Alexdelia <alexandre.delille.57@gmail.com>
2026-08-04 13:40:13 +0200

    feat: patch `complete-alias`

--- common/overlay.nix
+++ common/overlay.nix
@@ -1,8 +1,14 @@
-inputs: final: _: {
+inputs: final: prev: {
   unstable = import inputs.nixpkgs-unstable {
     inherit (final.stdenv.hostPlatform) system;
     inherit (final) config;
   };
+
+  complete-alias = prev.complete-alias.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      ./patch/complete-alias-quote-unset-array-subscript.patch
+    ];
+  });
 
   swhkd-no-rfkill =
     inputs.swhkd.packages.${final.stdenv.hostPlatform.system}.swhkd-no-rfkill.overrideAttrs
