1bb5b001bb5a6828961a31ffe0670d24b45283ab
Alexdelia <alexandre.delille.57@gmail.com>
2026-08-29 10:46:25 +0200

    chore: code quality

--- user/alex/home/script/kill-on-port.nix
+++ user/alex/home/script/kill-on-port.nix
@@ -1,16 +1,19 @@
 { pkgs, ... }:
 pkgs.writeShellApplication {
   name = "kill-on-port";
-  runtimeInputs = with pkgs; [
-    findutils
-    lsof
-  ];
+  runtimeInputs = with pkgs; [ lsof ];
   text = ''
     if [[ $# -lt 1 ]]; then
     	printf 'usage: \033[1m%s \033[35m<port>\033[0m\n' "$0"
     	exit 64 # sysexits.h `EX_USAGE` https://github.com/openbsd/src/blob/master/include/sysexits.h#L101
     fi
 
-    lsof -i:"$1" -sTCP:LISTEN -t | xargs -r kill || true
+    mapfile -t pid < <(lsof -i:"$1" -sTCP:LISTEN -t)
+
+    if [[ ''${#pid[@]} -eq 0 ]]; then
+    	exit 0
+    fi
+
+    kill "''${pid[@]}"
   '';
 }
