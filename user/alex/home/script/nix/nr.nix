be83ab1b88441ebe0001c49248ef8f339a596766
Alexdelia <alexandre.delille.57@gmail.com>
2026-08-29 10:28:39 +0200

    feat: migrate `nix`-related scripts to `writeShellApplication`

--- user/alex/home/script/nix/nr.nix
+++ user/alex/home/script/nix/nr.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
-(pkgs.writers.writeBashBin "nr" { } /* bash */ ''
-  if [[ $# -ne 1 ]]; then
-    printf "usage: \033[1m$0 \033[35m<pkg>\033[0m\n"
-    exit 64 # sysexits.h `EX_USAGE` https://github.com/openbsd/src/blob/master/include/sysexits.h#L101
-  fi
+pkgs.writeShellApplication {
+  name = "nr";
+  text = ''
+    if [[ $# -ne 1 ]]; then
+    	printf 'usage: \033[1m%s \033[35m<pkg>\033[0m\n' "$0"
+    	exit 64 # sysexits.h `EX_USAGE` https://github.com/openbsd/src/blob/master/include/sysexits.h#L101
+    fi
 
-  nix run nixpkgs#$1
-'')
+    nix run "nixpkgs#$1"
+  '';
+}
