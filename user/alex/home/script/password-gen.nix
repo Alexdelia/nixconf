73e72c0fee4df16477bbdb377c76f8e2aa5b4377
Alexdelia <alexandre.delille.57@gmail.com>
2026-08-29 10:28:48 +0200

    feat: migrate `password-gen` to `writeShellApplication`

--- user/alex/home/script/password-gen.nix
+++ user/alex/home/script/password-gen.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }:
-let
-  coreutils = pkgs.uutils-coreutils-noprefix;
-
-  tr = "${coreutils}/bin/tr";
-  head = "${coreutils}/bin/head";
-in
-pkgs.writers.writeBashBin "password-gen" { } /* bash */ ''
-  ${tr} -dc 'a-zABCDEFGHJKLMNPQRSTUVWXYZ1-9' < /dev/urandom | ${head} -c ''${1:-42}
-''
+pkgs.writeShellApplication {
+  name = "password-gen";
+  runtimeInputs = with pkgs; [ uutils-coreutils-noprefix ];
+  text = ''
+    tr -dc 'a-zABCDEFGHJKLMNPQRSTUVWXYZ1-9' < /dev/urandom | head -c "''${1:-42}" || true
+  '';
+}
