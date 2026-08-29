72f66a92fa2d01ee83aa6dbd5a2034ef8e6f0b0a
Alexdelia <alexandre.delille.57@gmail.com>
2026-08-29 10:27:19 +0200

    feat: migrate `rmpc` script to `writeShellApplication`

--- user/alex/home/music/mpd/client/rmpc/copy-uuid.nix
+++ user/alex/home/music/mpd/client/rmpc/copy-uuid.nix
@@ -13,22 +13,22 @@ pkgs.writeShellApplication {
   text = ''
     arg=()
     if [ "''${1:-selected}" = selected ]; then
-      while IFS= read -r path; do
-        [ -n "$path" ] || continue
-        arg+=(-p "$path")
-      done <<< "''${SELECTED_SONGS:-}"
+    	while IFS= read -r path; do
+    		[ -n "$path" ] || continue
+    		arg+=(-p "$path")
+    	done <<< "''${SELECTED_SONGS:-}"
     fi
 
     if ! song=$(rmpc song "''${arg[@]}"); then
-      notify-send -u critical "rmpc" "no song"
-      exit 1
+    	notify-send -u critical "rmpc" "no song"
+    	exit 1
     fi
 
     uuid=$(jaq -r '[.] | flatten | .[].metadata.musicbrainz_trackid // empty' <<< "$song")
 
     if [ -z "$uuid" ]; then
-      notify-send -u critical "rmpc" "no musicbrainz_trackid"
-      exit 1
+    	notify-send -u critical "rmpc" "no musicbrainz_trackid"
+    	exit 1
     fi
 
     ${clipboardCopy} "$uuid"
